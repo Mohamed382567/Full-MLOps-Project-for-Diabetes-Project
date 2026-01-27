6f5c29cec74683b60575cb51750c308ce985baf0
Mohamed Ahmed Elbaz <64mohamed2314@gmail.com>
2026-01-27 19:16:04 +0300

    Update Gradio app to use environment variable for port

--- src/app/gradio_app.py
+++ src/app/gradio_app.py
@@ -98,5 +98,12 @@ def build_ui():
 
 if __name__ == "__main__":
     app = build_ui()
-    # server_name="0.0.0.0" is mandatory for containerized environments
-    app.launch(server_name="0.0.0.0", server_port=7860)
+
+    import os
+    port = int(os.environ.get("PORT", 7860))
+    
+ app.launch(
+        server_name="0.0.0.0", 
+        server_port=port,   
+        share=False
+    )
